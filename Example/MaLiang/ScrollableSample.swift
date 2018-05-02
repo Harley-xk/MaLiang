4007839a541e952351206f5571ac2e474fdcc6b9
Harley-xk <harley.gb@foxmail.com>
2018-05-02 21:40:54 +0800

    scrollable sample

--- Example/MaLiang/ScrollableSample.swift
+++ Example/MaLiang/ScrollableSample.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class ScrollableSample: UIViewController {
 
+    @IBOutlet weak var scrollView: ScrollableCanvas!
+    @IBOutlet weak var canvas: Canvas!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let pencil = Brush(texture: #imageLiteral(resourceName: "pencil"))
+        pencil.pointSize = 3
+        pencil.pointStep = 2
+        pencil.opacity = 0.6
+        canvas.brush = pencil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
