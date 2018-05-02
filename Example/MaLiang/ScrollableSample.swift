b263b6f47be12ea322ac916e53576df35e5b5f02
Harley-xk <harley.gb@foxmail.com>
2018-05-02 21:40:30 +0800

    add ScrollableCanvas to support scrolling

---
+++ Example/MaLiang/ScrollableSample.swift
@@ -0,0 +1,35 @@
+//
+//  ScrollableSample.swift
+//  MaLiang_Example
+//
+//  Created by Harley.xk on 2018/5/2.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ScrollableSample: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MaLiang/Classes/ScrollableCanvas.swift
@@ -0,0 +1,21 @@
+//
+//  ScrollableCanvas.swift
+//  MaLiang_Example
+//
+//  Created by Harley.xk on 2018/5/2.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ScrollableCanvas: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
