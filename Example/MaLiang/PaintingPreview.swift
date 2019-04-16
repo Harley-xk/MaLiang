873a27bd740cdcb362a17aaae7c1fd8d3df81fa9
Harley-xk <harley.gb@foxmail.com>
2019-04-16 10:14:59 +0800

    add saving snapshot to album

--- Example/MaLiang/PaintingPreview.swift
+++ Example/MaLiang/PaintingPreview.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class PaintingPreview: UIViewController {
 
@@ -25,15 +26,56 @@ class PaintingPreview: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func savingAction(_ sender: Any) {
+        checkAblumAuthorized { (error) in
+            guard error == nil else {
+                self.alert(message: error?.localizedDescription)
+                return
+            }
+            self.performSavingAction()
+        }
     }
-    */
 
+    private func performSavingAction() {
+        guard let image = image else {
+            return alert(message: "Image does not exists")
+        }
+        
+        PHPhotoLibrary.shared().performChanges({
+            let data = image.pngData()!
+            let png = UIImage(data: data)!
+            PHAssetChangeRequest.creationRequestForAsset(from: png)
+        }) { (succeed, error) in
+            if let error = error {
+                self.alert(message: error.localizedDescription)
+            } else {
+                self.alert(message: "Image saved to album succeeded!")
+            }
+        }
+    }
+    
+    private func alert(message: String?) {
+        let alert = UIAlertController(title: "Tips", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    // MARK: - Album auth
+    
+    typealias FinishHandler = (Error?) -> ()
+
+    func checkAblumAuthorized(finished: @escaping FinishHandler) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch authorizationStatus {
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization { (_) in
+                self.checkAblumAuthorized(finished: finished)
+            }
+        case .denied: alert(message: "can not access album")
+        case .authorized: finished(nil)
+        case .restricted:
+            let error = NSError(domain: "Ablunm Authorization", code: -99, userInfo: [NSLocalizedDescriptionKey: "can not access album"])
+            finished(error)
+        }
+    }
 }
