dad0cff6dbdc5c24548285438bfb75486f367456
Harley-xk <harley.gb@foxmail.com>
2019-04-16 22:49:50 +0800

    add scroll indicators to scrollable canvas

--- MaLiang/Classes/ScrollableCanvas.swift
+++ MaLiang/Classes/ScrollableCanvas.swift
@@ -13,6 +13,8 @@ open class ScrollableCanvas: Canvas {
     open override func setup() {
         super.setup()
         
+        setupScrollIndicators()
+        
         contentSize = bounds.size
         
         pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGestureRecognizer(_:)))
@@ -36,7 +38,11 @@ open class ScrollableCanvas: Canvas {
     
     /// the actural drawable size of canvas, may larger than current bounds
     /// contentSize must between bounds size and 5120x5120
-    open var contentSize: CGSize = .zero
+    open var contentSize: CGSize = .zero {
+        didSet {
+            updateScrollIndicators()
+        }
+    }
     
     /// get snapthot image for the same size to content
     open override func snapshot() -> UIImage? {
@@ -74,6 +80,7 @@ open class ScrollableCanvas: Canvas {
         case .began:
             beginLocation = location
             offsetAnchor = location + contentOffset
+            showScrollIndicators()
         case .changed:
             guard gesture.numberOfTouches >= 2 else {
                 return
@@ -85,10 +92,12 @@ open class ScrollableCanvas: Canvas {
             let offset = offsetAnchor * (scale / currentZoomScale) - location
             contentOffset = offset.between(min: .zero, max: maxOffset)
             redraw()
+            updateScrollIndicators()
         case .ended: fallthrough
         case .cancelled: fallthrough
         case .failed:
             currentZoomScale = zoom
+            hidesScrollIndicators()
         default: break
         }
     }
@@ -98,17 +107,92 @@ open class ScrollableCanvas: Canvas {
         switch gesture.state {
         case .began:
             offsetAnchor = location + contentOffset
+            showScrollIndicators()
         case .changed:
             guard gesture.numberOfTouches >= 2 else {
                 return
             }
             contentOffset = (offsetAnchor - location).between(min: .zero, max: maxOffset)
             redraw()
-        default: break
+            updateScrollIndicators()
+        default: hidesScrollIndicators()
         }
     }
     
     private var maxOffset: CGPoint {
         return CGPoint(x: contentSize.width * zoom - bounds.width, y: contentSize.height * zoom - bounds.height)
+    }
+    
+    // MARK: - Scrolling Indicators
+    
+    /// show indicator while scrolling, like UIScrollView
+    
+    // defaults to true if width of contentSize is larger than bounds
+    open var showHorizontalScrollIndicator = true
+    
+    // defaults to true if height of contentSize is larger than bounds
+    open var showVerticalScrollIndicator = true
+    
+    private weak var horizontalScrollIndicator: UIView!
+    private weak var verticalScrollIndicator: UIView!
+    
+    private func setupScrollIndicators() {
+        
+        // horizontal scroll indicator
+        let horizontalScrollIndicator = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+        horizontalScrollIndicator.layer.cornerRadius = 2
+        horizontalScrollIndicator.clipsToBounds = true
+        addSubview(horizontalScrollIndicator)
+        self.horizontalScrollIndicator = horizontalScrollIndicator
+        
+        // vertical scroll indicator
+        let verticalScrollIndicator = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+        verticalScrollIndicator.layer.cornerRadius = 2
+        verticalScrollIndicator.clipsToBounds = true
+        addSubview(verticalScrollIndicator)
+        self.verticalScrollIndicator = verticalScrollIndicator
+    }
+    
+    private func updateScrollIndicators() {
+        
+        let showHorizontal = showHorizontalScrollIndicator && contentSize.width > bounds.width
+        horizontalScrollIndicator?.isHidden = !showHorizontal
+        if showHorizontal {
+            updateHorizontalScrollIndicator()
+        }
+        
+        let showVertical = showVerticalScrollIndicator && contentSize.height > bounds.height
+        verticalScrollIndicator.isHidden = !showVertical
+        if showVertical {
+            updateVerticalScrollIndicator()
+        }
+    }
+    
+    private func updateHorizontalScrollIndicator() {
+        let ratio = bounds.width / contentSize.width / zoom
+        let offsetRatio = contentOffset.x / contentSize.width / zoom
+        let width = bounds.width - 12
+        let frame = CGRect(x: offsetRatio * width + 4, y: bounds.height - 6, width: width * ratio, height: 4)
+        horizontalScrollIndicator.frame = frame
+    }
+    
+    private func updateVerticalScrollIndicator() {
+        let ratio = bounds.height / contentSize.height / zoom
+        let offsetRatio = contentOffset.y / contentSize.height / zoom
+        let height = bounds.height - 12
+        let frame = CGRect(x: bounds.width - 6, y: height * offsetRatio + 4, width: 4, height: height * ratio)
+        verticalScrollIndicator.frame = frame
+    }
+    
+    private func showScrollIndicators() {
+        horizontalScrollIndicator.alpha = 0.8
+        verticalScrollIndicator.alpha = 0.8
+    }
+    
+    private func hidesScrollIndicators() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut, .beginFromCurrentState], animations: {
+            self.horizontalScrollIndicator.alpha = 0
+            self.verticalScrollIndicator.alpha = 0
+        })
     }
 }
