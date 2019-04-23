224053fdea7cd0685f1c4f7f0fa51f999265c4d0
Harley-xk <harley.gb@foxmail.com>
2019-04-23 16:19:47 +0800

    add chrysan to example

--- Example/Pods/Target Support Files/Pods-MaLiang_Example/Pods-MaLiang_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MaLiang_Example/Pods-MaLiang_Example-frameworks.sh
@@ -153,10 +153,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Chrysan/Chrysan.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Comet/Comet.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MaLiang/MaLiang.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Chrysan/Chrysan.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Comet/Comet.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MaLiang/MaLiang.framework"
 fi
